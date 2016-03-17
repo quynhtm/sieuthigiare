94726aaf5cc9b4bce5c1dd2bc2c4f39f0239c31a
Trương Mạnh Quỳnh <quynhtruongmanh@vccorp.vn>
2016-03-17 09:12:16 +0700

    push con lần 1

---
+++ sites/all/themes/theme_default/page/html.tpl.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title><?php print t($head_title); ?></title>
+  <?php print t($head); ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65301938-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<meta name="google-site-verification" content="gcFtwgAqRiGIMAcISuSG0tF2ngSUTGNXoFuo-6_rHCs" />
+</head>
+<body id="<?php print strtolower($body_id); ?>" class="<?php print $classes; ?> body" <?php print $attributes;?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
---
+++ sites/all/themes/theme_default/page/page.tpl.php
@@ -0,0 +1,31 @@
+<div id="wrapper">
+	<?php if ($page['header']){ ?>
+		<div id="header">
+			<?php echo render($page['header']); ?>
+		</div>
+	<?php } ?>
+	<div id="content">
+		<div class="wrapper-content">
+			<?php if(!isset($messages)) $messages = ''; echo $messages; ?>
+			<div <?php if ($page['right']){ ?>class="site-left"<?php }else{ ?> class="site-content"<?php } ?>>
+				<?php if ($page['left']){ ?>
+					<?php echo render($page['left']); ?>
+				<?php } ?>
+				<?php if ($page['content']){ ?>
+					<?php unset($page['content']['system_main']['default_message']); ?>
+			    	<?php echo render($page['content']); ?>
+				<?php } ?>
+			</div>				
+			<?php if ($page['right']){ ?>
+				<div class="site-right">
+				<?php echo render($page['right']); ?>
+				</div>
+			<?php } ?>
+		</div>	
+	</div>
+	<?php if ($page['footer']){ ?>
+	    <div id="footer">
+			<?php echo render($page['footer']); ?>
+		</div>
+	<?php } ?>
+</div>
