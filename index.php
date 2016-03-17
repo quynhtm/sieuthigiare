72064501b145a10e4188b85e2021d679209c4054
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-03-17 13:31:55 +0700

    duynx

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
-
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
- */
-
-/**
- * Root directory of Drupal installation.
- */
-define('DRUPAL_ROOT', getcwd());
-
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-menu_execute_active_handler();
+try{
+	define('DRUPAL_ROOT', getcwd());
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+	drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+	menu_execute_active_handler();
+}catch(Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
