3e5d423ca0d6e5e14465e1b7823f5040655d1bee
Trương Mạnh Quỳnh <quynhtruongmanh@vccorp.vn>
2016-03-25 16:53:10 +0700

    sua menu

--- sites/all/modules/backend/View/tpl/block/admin-left.tpl.php
+++ sites/all/modules/backend/View/tpl/block/admin-left.tpl.php
@@ -14,26 +14,26 @@
         	<a title="<?php echo t('Bảng điều khiển')?>" href="<?php echo $base_url?>/admincp"><i class="icon-dashboard"></i>Trang chủ Admin</a>
         </li>
         <li <?php if($param[1]=='province' || $param[1]=='configinfo'|| $param[1]=='supportonline'|| $param[1]=='supplier'){?> class="active" <?php } ?>>
-            <a title="" href="javascript:void(0)"><i class="icon-files-o"></i>Quản lý Ứng dụng<i class="icon-arrow"></i></a>
+            <a title="" href="javascript:void(0)"><i class="icon-folder-open"></i> Quản lý Ứng dụng<i class="icon-arrow"></i></a>
             <ul class="sub">
-                <li <?php if($param[1]=='configinfo'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/configinfo"><i class="icon-circle-o"></i>Cấu hình chung</a></li>
-                <li <?php if($param[1]=='province'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/province"><i class="icon-circle-o"></i>Quản lý tỉnh/thành</a></li>
-                <li <?php if($param[1]=='supportonline'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/supportonline"><i class="icon-circle-o"></i>Nick support</a></li>
-                <li <?php if($param[1]=='supplier'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/supplier"><i class="icon-circle-o"></i>Danh sách NCC</a></li>
+                <li <?php if($param[1]=='configinfo'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/configinfo"><i class="icon-minus"></i> Cấu hình chung</a></li>
+                <li <?php if($param[1]=='province'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/province"><i class="icon-minus"></i> Quản lý tỉnh/thành</a></li>
+                <li <?php if($param[1]=='supportonline'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/supportonline"><i class="icon-minus"></i> Nick support</a></li>
+                <li <?php if($param[1]=='supplier'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/supplier"><i class="icon-minus"></i> Danh sách NCC</a></li>
             </ul>
         </li>
 
         <li <?php if($param[1]=='product' || $param[1]=='product_category' || $param[1]=='product_comment'){?> class="active" <?php } ?>>
-            <a title="" href="javascript:void(0)"><i class="icon-files-o"></i>Quản lý Sản phẩm<i class="icon-arrow"></i></a>
+            <a title="" href="javascript:void(0)"><i class="icon-cogs"></i> Quản lý Sản phẩm<i class="icon-arrow"></i></a>
             <ul class="sub">
-                <li <?php if($param[1]=='product'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product"><i class="icon-circle-o"></i>Sản phẩm</a></li>
-                <li <?php if($param[1]=='product_category'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product_category"><i class="icon-circle-o"></i>Danh mục sản phẩm</a></li>
-                <li <?php if($param[1]=='product_comment'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product_comment"><i class="icon-circle-o"></i>QL Bình luận</a></li>
+                <li <?php if($param[1]=='product'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product"><i class="icon-minus"></i> Sản phẩm</a></li>
+                <li <?php if($param[1]=='product_category'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product_category"><i class="icon-minus"></i> Danh mục sản phẩm</a></li>
+                <li <?php if($param[1]=='product_comment'){?> class="active" <?php } ?>><a class="" title="" href="<?php echo $base_url ?>/admincp/product_comment"><i class="icon-minus"></i> QL Bình luận</a></li>
             </ul>
         </li>
 
         <li <?php if($param[1]=='usershop'){?> class="active" <?php } ?>>
-            <a title="" href="<?php echo $base_url ?>/admincp/usershop"><i class="icon-files-o"></i>Quản trị User Shop</a>
+            <a title="" href="<?php echo $base_url ?>/admincp/usershop"><i class="icon-home"></i> Quản trị User Shop</a>
         </li>
     </ul>
 </div>
