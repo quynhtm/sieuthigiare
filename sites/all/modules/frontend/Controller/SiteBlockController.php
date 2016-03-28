f1da1023890d34c407bb78e85a1ddc91327b2315
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-03-28 21:32:26 +0700

    duy

--- sites/all/modules/frontend/Controller/SiteBlockController.php
+++ sites/all/modules/frontend/Controller/SiteBlockController.php
@@ -5,19 +5,3 @@
 * @Date 	 : 06/2014
 * @Version	 : 1.0
 */
-function block_header(){
-	$view= theme('block-header');
-	return $view;
-}
-function block_slide(){
-	$view= theme('block-slide');
-	return $view;
-}
-function block_content(){
-	$view= theme('block-content');
-	return $view;
-}
-function block_footer(){
-	$view= theme('block-footer');
-	return $view;
-}
