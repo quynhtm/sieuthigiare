dbb0301160faf092394e317c559caa37defb7054
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-03-31 11:49:27 +0700

    duy

---
+++ sites/all/modules/autoLoad/Common.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+* @Created by: HSS
+* @Author	 : nguyenduypt86@gmail.com
+* @Date 	 : 06/2014
+* @Version	 : 1.0 
+*/
+class Common(){
+	public static $table_action_category = TABLE_CATEGORY;
+	
+	public static $table_action_province = TABLE_PROVINCE;
+	public static static $primary_key_category = 'province_id';
+	
+	public static getListCategory($parent_id=0, $sub=0){
+		//code here
+
+	}
+}
