da92de1d0689516902722b1d4df71386fd83d9ee
Trương Mạnh Quỳnh <quynhtruongmanh@vccorp.vn>
2016-03-31 11:56:39 +0700

    suwa class

--- sites/all/modules/autoLoad/Common.php
+++ sites/all/modules/autoLoad/Common.php
@@ -5,11 +5,11 @@
 * @Date 	 : 06/2014
 * @Version	 : 1.0 
 */
-class Common(){
+class Common{
 	public static $table_action_category = TABLE_CATEGORY;
-	
 	public static $table_action_province = TABLE_PROVINCE;
-	public static static $primary_key_category = 'province_id';
+
+	public static $primary_key_category = 'province_id';
 	
 	public static function getListCategory($parent_id=0, $sub=0){
 		//code here
