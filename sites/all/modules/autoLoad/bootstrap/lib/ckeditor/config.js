b179fd022fcb419a8e6965ca415a0f47b0b4a710
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-03 18:59:19 +0700

    duy

--- sites/all/modules/autoLoad/bootstrap/lib/ckeditor/config.js
+++ sites/all/modules/autoLoad/bootstrap/lib/ckeditor/config.js
@@ -10,7 +10,7 @@ CKEDITOR.editorConfig = function( config ) {
     config.disallowedContent = 'script{*}';
 	config.extraPlugins = 'youtube';
 
-    /*config.toolbar = [
+    config.toolbar = [
         { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
         { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
         { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
@@ -19,14 +19,14 @@ CKEDITOR.editorConfig = function( config ) {
         { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
         { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
         { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-        { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+        { name: 'insert', items: [ /*'Image', 'Flash',*/ 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
         '/',
         { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
         { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
         { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
         { name: 'others', items: [ '-' ] },
         { name: 'about', items: [ 'About' ] }
-    ];*/
+    ];
 
     /*config.toolbar = 'MyToolbar';
     config.toolbar_MyToolbar =
