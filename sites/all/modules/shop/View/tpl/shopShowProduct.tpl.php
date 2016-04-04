0f0ffd7382daab041c5029275e67227c6641cdee
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-04 17:11:21 +0700

    duy

--- sites/all/modules/shop/View/tpl/shopShowProduct.tpl.php
+++ sites/all/modules/shop/View/tpl/shopShowProduct.tpl.php
@@ -2,51 +2,215 @@
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="link-breadcrumb">
+				<a href="">Trang chủ</a><i class="icon-double-angle-right"></i>
+				<a href="">Điện máy & công nghệ</a>
+			</div>
 			<div class="main-view-post box-register">
 				<div class="wrap-main-view">
-					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-						<div class="left-category-shop">
-							<div class="title-category-parent">ĐIỆN MÁY & CÔNG NGHỆ</div>
-							<ul>
-								<li><a href="">Điện thoại - Máy tính bảng</a></li>
-								<li><a href="">Máy tính - Linh phụ kiện</a></li>
-								<li><a href="">Thiết bị nhà bếp</a></li>
-								<li><a href="">Thiết bị an ninh</a></li>
-								<li><a href="">Điện tử - Kỹ thuật số</a></li>
-								<li><a href="">Điện thoại - Máy tính bảng</a></li>
-								<li><a href="">Điện máy - Điện lạnh - Điện gia dụng</a></li>
-							</ul>
-						</div>
+					<div class="left-category-shop">
+						<div class="title-category-parent">ĐIỆN MÁY & CÔNG NGHỆ</div>
+						<ul>
+							<li><a href="">Điện thoại - Máy tính bảng</a></li>
+							<li><a href="">Máy tính - Linh phụ kiện</a></li>
+							<li><a href="">Thiết bị nhà bếp</a></li>
+							<li><a href="">Thiết bị an ninh</a></li>
+							<li><a href="">Điện tử - Kỹ thuật số</a></li>
+							<li><a href="">Điện thoại - Máy tính bảng</a></li>
+							<li><a href="">Điện máy - Điện lạnh - Điện gia dụng</a></li>
+						</ul>
 					</div>
-					<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-						<div class="right-show-product-shop body-list-item ">
-							<div class="col-lg-4 col-xs-4">
-								<div class="item">
-									<div class="post-thumb">
-										<img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/1.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)">
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title"><a title="Cốc tự nguấy Self Stirring MUG 2015 (Đen)" href="">Cốc tự nguấy Self Stirring MUG 2015 (Đen)</a></h4>
-											<div class="item-price">
-	                                    		<span class="amount-1">99.000đ</span>
-												<span class="amount-2">160.000đ</span>
-                                    			<span class="sale-off">-38%</span>
-                                    		</div>
+					<div class="right-show-product-shop body-list-item ">
+							<div class="banner-shop-content">
+								<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/banner.jpg" alt=""></a>
+							</div>
+							<div class="content-list-item">
+								<div class="col-lg-3 col-xs-3 ">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/1.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
 										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div class="rating" style="width:0%" id="rate-1"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">
-				                        	
-				                        </div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Cốc tự nguấy Self Stirring MUG 2015 (Đen)" href="">Cốc tự nguấy Self Stirring MUG 2015 (Đen)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">99.000đ</span>
+													<span class="amount-2">160.000đ</span>
+	                                    			<span class="sale-off">-38%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/2.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Bộ 5 nồi inox ba đáy nắp kiếng Five Star FS08CG1 (Bạc)" href="">Bộ 5 nồi inox ba đáy nắp kiếng Five Star FS08CG1 (Bạc)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">685.000đ</span>
+													<span class="amount-2">755.000đ</span>
+	                                    			<span class="sale-off">-9%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/3.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Bộ 12 món chén đĩa Luminarc Essence J4852 - Tặng bộ ly thủy tinh Fillon D0545" href="">Bộ 12 món chén đĩa Luminarc Essence J4852 - Tặng bộ ly thủy tinh Fillon D0545</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">440.000đ </span>
+													<span class="amount-2">900.000đ</span>
+	                                    			<span class="sale-off">-51%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/4.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Máy đánh trứng cầm tay USA Store CapuChino (Xanh)" href="">Máy đánh trứng cầm tay USA Store CapuChino (Xanh)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">99.000đ</span>
+													<span class="amount-2">190.000đ</span>
+	                                    			<span class="sale-off">-51%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3 ">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/1.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Cốc tự nguấy Self Stirring MUG 2015 (Đen)" href="">Cốc tự nguấy Self Stirring MUG 2015 (Đen)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">99.000đ</span>
+													<span class="amount-2">160.000đ</span>
+	                                    			<span class="sale-off">-38%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/2.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Bộ 5 nồi inox ba đáy nắp kiếng Five Star FS08CG1 (Bạc)" href="">Bộ 5 nồi inox ba đáy nắp kiếng Five Star FS08CG1 (Bạc)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">685.000đ</span>
+													<span class="amount-2">755.000đ</span>
+	                                    			<span class="sale-off">-9%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/3.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Bộ 12 món chén đĩa Luminarc Essence J4852 - Tặng bộ ly thủy tinh Fillon D0545" href="">Bộ 12 món chén đĩa Luminarc Essence J4852 - Tặng bộ ly thủy tinh Fillon D0545</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">440.000đ </span>
+													<span class="amount-2">900.000đ</span>
+	                                    			<span class="sale-off">-51%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-3 col-xs-3">
+									<div class="item">
+										<div class="post-thumb">
+											<a href="" title=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/4.jpg" alt="Cốc tự nguấy Self Stirring MUG 2015 (Đen)"></a>
+										</div>
+										<div class="item-content">
+											<div class="title-info">
+												<h4 class="post-title"><a title="Máy đánh trứng cầm tay USA Store CapuChino (Xanh)" href="">Máy đánh trứng cầm tay USA Store CapuChino (Xanh)</a></h4>
+												<div class="item-price">
+		                                    		<span class="amount-1">99.000đ</span>
+													<span class="amount-2">190.000đ</span>
+	                                    			<span class="sale-off">-51%</span>
+	                                    		</div>
+											</div>
+											<div class="item-rating">
+					                            <div class="rating-box">
+					                                <div class="rating" style="width:0%" id="rate-1"></div>
+					                            </div>
+					                        </div>
+					                        <div class="item-banner"></div>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 				</div>
 			</div>
 		</div>
