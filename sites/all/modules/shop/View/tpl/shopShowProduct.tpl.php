909ef10be53bc943e056987aba632205640d1fe2
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-04 09:39:16 +0700

    duy

---
+++ sites/all/modules/shop/View/tpl/shopShowProduct.tpl.php
@@ -0,0 +1,67 @@
+<?php global $base_url;?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="main-view-post box-register">
+				<div class="wrap-main-view">
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+						<div class="left-category-shop">
+							<div class="title-category-parent">ĐIỆN MÁY & CÔNG NGHỆ</div>
+							<ul>
+								<li><a href="">Điện thoại - Máy tính bảng</a></li>
+								<li><a href="">Máy tính - Linh phụ kiện</a></li>
+								<li><a href="">Thiết bị nhà bếp</a></li>
+								<li><a href="">Thiết bị an ninh</a></li>
+								<li><a href="">Điện tử - Kỹ thuật số</a></li>
+								<li><a href="">Điện thoại - Máy tính bảng</a></li>
+								<li><a href="">Điện máy - Điện lạnh - Điện gia dụng</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+						<div class="right-show-product-shop body-list-item ">
+							<div class="col-lg-4 col-xs-4">
+								<div class="item">
+									<div class="post-thumb">
+										<img src="https://cdn02.static-adayroi.com/resize/240_240/100/0/2016/03/09/1457494557934_5874653.jpg" 
+										alt="USB 2.0 Toshiba Hayabusa 16GB Trắng" data-other-src="https://cdn02.static-adayroi.com/resize/240_240/100/0/2016/03/09/1457494558533_4904465.jpg" 
+										class="lazy imglist" style="display: inline-block;">
+									</div>
+									<div class="item-content">
+										<div class="title-info">
+											<h4 class="post-title"><a title="USB 2.0 Toshiba Hayabusa 16GB Trắng" href="/usb-2-0-toshiba-hayabusa-16gb-trang-p-ppQKG-f1-2?pi=DzRLl&amp;w=K6ll">USB 2.0 Toshiba Hayabusa 16GB Trắng</a></h4>
+											<div class="item-price">
+	                                    		<span class="amount-1">99.000đ</span>
+												<span class="amount-2">160.000đ</span>
+                                    			<span class="sale-off">-38%</span>
+                                    		</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4 col-xs-4">
+								<div class="item">
+									<div class="post-thumb">
+										<img src="https://cdn02.static-adayroi.com/resize/240_240/100/0/2016/03/09/1457494557934_5874653.jpg" 
+										alt="USB 2.0 Toshiba Hayabusa 16GB Trắng" data-other-src="https://cdn02.static-adayroi.com/resize/240_240/100/0/2016/03/09/1457494558533_4904465.jpg" 
+										class="lazy imglist" style="display: inline-block;">
+									</div>
+									<div class="item-content">
+										<div class="title-info">
+											<h4 class="post-title"><a title="USB 2.0 Toshiba Hayabusa 16GB Trắng" href="/usb-2-0-toshiba-hayabusa-16gb-trang-p-ppQKG-f1-2?pi=DzRLl&amp;w=K6ll">USB 2.0 Toshiba Hayabusa 16GB Trắng</a></h4>
+											<div class="item-price">
+	                                    		<span class="amount-1">99.000đ</span>
+												<span class="amount-2">160.000đ</span>
+                                    			<span class="sale-off">-38%</span>
+                                    		</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
