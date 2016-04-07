832f8b77ecd4d1648b753b19258750e323e793d7
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-07 08:52:55 +0700

    duy

---
+++ sites/all/modules/shop/View/tpl/shopDetailProduct.tpl.php
@@ -0,0 +1,68 @@
+<?php global $base_url;?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="link-breadcrumb">
+				<a href="">Trang chủ</a><i class="icon-double-angle-right"></i>
+				<a href="">Điện máy & công nghệ</a>
+			</div>
+			<div class="main-view-post box-detail-product">
+				<div class="wrap-main-view">
+					<div class="tob-content-view">
+						<div class="left-slider-img">
+							<ul class="list-thumb-img">
+								<li><a href=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/s1.jpg" alt=""></a></li>
+								<li><a href=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/s2.jpg" alt=""></a></li>
+								<li><a href=""><img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/s3.jpg" alt=""></a></li>
+							</ul>
+							<div class="max-thumb-img">
+								<img src="<?php echo $base_url.'/'.path_to_theme()?>/View/img/s1.jpg" alt="">
+							</div>
+						</div>
+						<div class="center-des-product">
+							<h1>Áo Sơ Mi Nam Công Sở Fonto Menswear : SR048</h1>
+							<div class="row-price">
+								<div class="lbl-row">Giá thị trường:</div>
+								<div class="price-origin">2.849.000<span class="td-border">đ</span></div>
+							</div>
+							<div class="row-price">
+								<div class="lbl-row lbl-price-sale">Giá bán:</div>
+								<div class="price-sale">2.419.000<span class="td-border">đ</span></div>
+							</div>
+							<div class="features-point">
+								<div class="lbl-point">Mô tả sản phẩm</div>
+								<div class="des-point">
+									Áo sơ mi nam thương hiệu FONTO Menswear sử dụng chất liệu Cotton-Bamboo, form dáng Hàn Quốc<br>
+									-Với chất liệu thoáng mát, thấm mồ hôi và mềm mại tự nhiên, đem đến sự thoải mái khi mang mặc.<br>
+									- Kiểu dáng trang nhã toát lên sự lịch lãm,sang trọng.<br>
+									- Chuẩn mực mới cho thời trang công sở.<br>
+									- Chất liệu 80% bamboo, 20% poly<br>
+									- Cổ lật<br>
+									- May hàng nút cài dọc thân áo trước<br>
+									- Thiết kế may túi bên ngực trái và thêu logo thương hiệu trên miệng túi<br>
+									- Dáng Regular<br>
+									- Không co giãn<br>
+								</div>
+							</div>
+						</div>
+						<div class="right-des-product">
+							<div class="order-number">
+								<label for="buy-number">Số lượng</label>
+								<select class="sl-number" id="buy-number" name="">
+	                                <option value="1">1</option>
+	                            </select>
+							</div>
+							<div class="buynow">Mua ngay</div>
+						</div>
+					</div>
+					<div class="bottom-content-view">
+						<ul class="tab">
+							<li>Chi tiết sản phẩm</li>
+							<li>Nhận xét</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
