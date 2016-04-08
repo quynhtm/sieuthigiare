b508969b44aaab6d7339a4820a6949dff22b2649
trương mạnh quỳnh <quynhtruongmanh@vccorp.vn>
2016-04-08 23:27:20 +0700

    thêm DB order và contact

---
+++ sites/all/modules/backend/Model/Contact.php
@@ -0,0 +1,114 @@
+<?php
+/*
+* QuynhTM
+*/
+class Contact{
+	static $table_action = TABLE_CONTACT;
+	static $primary_key = 'contact_id';
+	static $arrFields = array('contact_id','contact_title', 'contact_content','contact_phone_send', 'contact_email_send',
+		'contact_type','contact_reason', 'contact_status','contact_user_id_send', 'contact_user_name_send',
+		'contact_time_creater', 'contact_time_update');
+
+	public static function getSearchListItems($dataSearch = array(), $limit = 30, $arrFields = array()){
+		//n?u get field rong thi lay all
+		if(empty($arrFields))
+			$arrFields = self::$arrFields;
+
+        if(!empty($arrFields)){
+            $sql = db_select(self::$table_action, 'i')->extend('PagerDefault');
+            foreach($arrFields as $field){
+                $sql->addField('i', $field, $field);
+            }
+
+            /*Begin search*/
+            $cond = '';
+            $arrCond = array();
+			if(!empty($dataSearch)){
+				foreach($dataSearch as $field =>$value){
+					if($field === 'contact_type' && $value != -1){
+						$sql->condition('i.'.$field, $value, '=');
+						array_push($arrCond, $field.' = '.$value);
+					}
+					if($field === 'contact_status' && $value != -1){
+						$sql->condition('i.'.$field, $value, '=');
+						array_push($arrCond, $field.' = '.$value);
+					}
+					if($field === 'contact_title' && $value != ''){
+						$db_or = db_or();
+						$db_or->condition('i.'.$field, '%'.$value.'%', 'LIKE');
+						$sql->condition($db_or);
+						array_push($arrCond, "(".$field." LIKE '%". $value ."%')");
+					}
+				}
+				if(!empty($arrCond)){
+					$cond = implode(' AND ', $arrCond);
+				}
+			}
+            /*End search*/
+
+            $totalItem = DB::countItem(self::$table_action, self::$primary_key , $cond, '', self::$primary_key.' ASC');
+            $result = $sql->limit($limit)->orderBy('i.'.self::$primary_key, 'DESC')->execute();
+            $arrItem = (array)$result->fetchAll();
+
+            $pager = array('#theme' => 'pager','#quantity' => 3);
+            $data['data'] = $arrItem;
+            $data['pager'] = $pager;
+            $data['total'] = $totalItem;
+			return $data;
+        }
+        return array('data' => array(),'total' => 0,'pager' => array(),);
+    }
+
+	public static function getItemById($arrFields = array(), $id = 0){
+		$result = array();
+		if($id > 0){
+			$arrFields = !empty($arrFields)? $arrFields : self::$arrFields;
+			$result = DB::getItemById(self::$table_action,self::$primary_key, $arrFields, $id);
+		}
+		return !empty($result)? $result[0]: array();
+	}
+
+	public static function save($data=array(), $id = 0){
+		$data_post = array();
+		if(!empty($data)){
+			foreach($data as $key=>$val){
+				$data_post[$key] = $val['value'];
+			}
+		}
+		//update
+		if($id > 0){
+			self::updateId($data_post, $id);
+			drupal_set_message('Upadate success.');
+			return true;
+		}
+		//insert
+		else{
+			$query = self::insert($data_post);
+			if($query){
+				drupal_set_message('Insert success.');
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function insert($dataInsert){
+		if(!empty($dataInsert)){
+			return DB::insertOneItem(self::$table_action, $dataInsert);
+		}
+		return false;
+	}
+	public static function updateId($dataUpdate, $id = 0){
+		if($id > 0 && !empty($dataUpdate)){
+			return DB::updateId(self::$table_action, self::$primary_key, $dataUpdate, $id);
+		}
+		return false;
+	}
+
+	public static function deleteId($id){
+		if($id > 0){
+			return DB::deleteId(self::$table_action, self::$primary_key, $id);
+		}
+		return false;
+	}
+}
