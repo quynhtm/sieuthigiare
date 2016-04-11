e371fec7855fca213b419c14ba4aad68794a3230
Trương Mạnh Quỳnh <quynhtruongmanh@vccorp.vn>
2016-04-11 15:27:09 +0700

    them ham lấy dữ liệu cho shop

--- sites/all/modules/autoLoad/DataCommon.php
+++ sites/all/modules/autoLoad/DataCommon.php
@@ -8,6 +8,7 @@
 class DataCommon{
 	public static $table_category = TABLE_CATEGORY;
 	public static $table_province = TABLE_PROVINCE;
+	public static $table_user_shop = TABLE_USER_SHOP;
 
 	public static function getListCategoryParent(){
 		$query = db_select(self::$table_category, 'c')
@@ -19,6 +20,30 @@ class DataCommon{
 		if(!empty($data)){
 			foreach($data as $k=> $cate){
 				$result[$cate->category_id] = $cate->category_name;
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * @param int $id_shop
+	 * @return array
+	 */
+	public static function getShopById($id_shop = 0){
+		if($id_shop <= 0) return array();
+		$result = array();
+		if(!empty($result)){
+			//sau vi?t get cache ? ?ây
+		}else{
+			$query = db_select(self::$table_user_shop, 's')
+				->condition('s.shop_id', $id_shop, '=')
+				->condition('s.shop_status', 1, '=')
+				->fields('s');
+			$data = $query->execute();
+			if(!empty($data)){
+				foreach($data as $k=> $cate){
+					$result[$cate->shop_id] = $cate;
+				}
 			}
 		}
 		return $result;
