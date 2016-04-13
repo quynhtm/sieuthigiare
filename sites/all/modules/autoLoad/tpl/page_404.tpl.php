e65a20da724de4d28caaef5e31de8e4e20ae0267
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-13 16:48:27 +0700

    duy

--- sites/all/modules/autoLoad/tpl/page_404.tpl.php
+++ sites/all/modules/autoLoad/tpl/page_404.tpl.php
@@ -1,4 +1,3 @@
-<?php global $base_url;?>
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -6,7 +5,6 @@
 				<div class="wrap-main-view">
 					<div class="view-content-static">
 						<div class="page-access">Không tìm thấy trang này...</div>
-						<?php header('Refresh: 5; url="'.$base_url.'"'); ?>
 					</div>
 				</div>
 			</div>
