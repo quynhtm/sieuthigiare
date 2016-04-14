d241c94e715d2a24eb2ae2354dab5e6d380b761e
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-14 19:04:48 +0700

    duy

--- sites/all/modules/autoLoad/DataCommon.php
+++ sites/all/modules/autoLoad/DataCommon.php
@@ -12,8 +12,6 @@ class DataCommon{
 	public static $table_product = TABLE_PRODUCT;
 	public static $primary_key_province = 'province_id';
 
-	const CACHE_ON = 0;
-
 	public static function getListCategoryParent(){
 		$query = db_select(self::$table_category, 'c')
 			->condition('c.category_parent_id', 0, '=')
