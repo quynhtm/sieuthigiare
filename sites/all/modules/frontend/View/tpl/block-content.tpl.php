42d552d10be548f3796cb46bbc29b272beb333aa
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-16 12:24:56 +0700

    duy

--- sites/all/modules/frontend/View/tpl/block-content.tpl.php
+++ sites/all/modules/frontend/View/tpl/block-content.tpl.php
@@ -2,36 +2,72 @@
 	global $base_url;
 ?>
 <div class="container">
-	<div class="title-list-item"><span>Laptop - Máy tính bảng</span></div>
-	<div class="content-list-item w-home">
-		<ul>
-			<?php for($i=0; $i<10; $i++){ ?>
-			<li class="item">
-				<div class="post-thumb">
-					<a title="" href="">
-						<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" 
-						src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-36-42-12-04-2016-best-smartphone.jpg">
-					</a>
-				</div>
-				<div class="item-content">
-					<div class="title-info">
-						<h4 class="post-title">
-							<a href="" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-						</h4>
-						<div class="item-price">
-	                	<span class="amount-call">Liên hệ: <i class="num-call">0913922986</i></span>
-	                </div>
+	<div class="w-list-block">
+		<div class="title-list-item">Laptop - Máy tính bảng</div>
+		<div class="content-list-item w-home">
+			<ul>
+				<?php for($i=0; $i<10; $i++){ ?>
+				<li class="item">
+					<div class="post-thumb">
+						<a title="" href="">
+							<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" 
+							src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-36-42-12-04-2016-best-smartphone.jpg">
+						</a>
 					</div>
-					<div class="item-rating">
-	                    <div class="rating-box">
-	                        <div id="rate-1" style="width:0%" class="rating"></div>
-	                    </div>
-	                </div>
-	                <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-				</div>
-			</li>
-			<?php } ?>
-		</ul>
-	</div>	
+					<div class="item-content">
+						<div class="title-info">
+							<h4 class="post-title">
+								<a href="" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
+							</h4>
+							<div class="item-price">
+		                	<span class="amount-call">Liên hệ: <i class="num-call">0913922986</i></span>
+		                </div>
+						</div>
+						<div class="item-rating">
+		                    <div class="rating-box">
+		                        <div id="rate-1" style="width:0%" class="rating"></div>
+		                    </div>
+		                </div>
+		                <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
+					</div>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>	
+	</div>
+
+	<div class="w-list-block">
+		<div class="title-list-item">Laptop - Máy tính bảng</div>
+		<div class="content-list-item w-home">
+			<ul>
+				<?php for($i=0; $i<10; $i++){ ?>
+				<li class="item">
+					<div class="post-thumb">
+						<a title="" href="">
+							<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" 
+							src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-36-42-12-04-2016-best-smartphone.jpg">
+						</a>
+					</div>
+					<div class="item-content">
+						<div class="title-info">
+							<h4 class="post-title">
+								<a href="" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
+							</h4>
+							<div class="item-price">
+		                	<span class="amount-call">Liên hệ: <i class="num-call">0913922986</i></span>
+		                </div>
+						</div>
+						<div class="item-rating">
+		                    <div class="rating-box">
+		                        <div id="rate-1" style="width:0%" class="rating"></div>
+		                    </div>
+		                </div>
+		                <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
+					</div>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>	
+	</div>
 </div>
 
