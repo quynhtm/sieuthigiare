6bd8b99bc3ac4e4eba5ff1f2fe672ec3a2a9348d
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-16 12:18:39 +0700

    duy

--- sites/all/modules/frontend/View/tpl/block-content.tpl.php
+++ sites/all/modules/frontend/View/tpl/block-content.tpl.php
@@ -2,148 +2,36 @@
 	global $base_url;
 ?>
 <div class="container">
-	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			
-			<div class="content-list-item">
-								<div class="col-lg-3 col-xs-3 ">
-								<div class="item">
-									<div class="post-thumb">
-										<a title="Điện thoại di động LG G3 16GB D855" href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p31/Dien-thoai-di-dong-LG-G3-16GB-D855.html">
-											<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg">
-										</a>
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title">
-												<a href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p31/Dien-thoai-di-dong-LG-G3-16GB-D855.html" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-											</h4>
-											<div class="item-price">
-	                                    			                                    			<span class="amount-call">Liên hệ: <i class="num-call">0913922986</i></span>
-	                                    		                                    		</div>
-										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div id="rate-1" style="width:0%" class="rating"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-									</div>
-								</div>
-							</div>
-														<div class="col-lg-3 col-xs-3 ">
-								<div class="item">
-									<div class="post-thumb">
-										<a title="Điện thoại di động LG G3 16GB D855" href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p30/Dien-thoai-di-dong-LG-G3-16GB-D855.html">
-											<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=30&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-36-42-12-04-2016-best-smartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=30&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-38-28-12-04-2016-lgg3androidsmartphone.jpg">
-										</a>
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title">
-												<a href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p30/Dien-thoai-di-dong-LG-G3-16GB-D855.html" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-											</h4>
-											<div class="item-price">
-	                                    				                                    			                                    		                                    		</div>
-										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div id="rate-1" style="width:0%" class="rating"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-									</div>
-								</div>
-							</div>
-														<div class="col-lg-3 col-xs-3 ">
-								<div class="item">
-									<div class="post-thumb">
-										<a title="Điện thoại di động LG G3 16GB D855" href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p29/Dien-thoai-di-dong-LG-G3-16GB-D855.html">
-											<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=29&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/29/01-36-42-12-04-2016-best-smartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=29&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/29/01-38-28-12-04-2016-lgg3androidsmartphone.jpg">
-										</a>
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title">
-												<a href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p29/Dien-thoai-di-dong-LG-G3-16GB-D855.html" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-											</h4>
-											<div class="item-price">
-	                                    				                                    				                                    		<span class="amount-1">7,350,000đ</span>
-													<span class="amount-2">8,500,000đ</span>
-																									    <span class="sale-off">
-												    	-13.5%
-												    </span>
-												    											    	                                    		                                    		</div>
-										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div id="rate-1" style="width:0%" class="rating"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-									</div>
-								</div>
-							</div>
-														<div class="col-lg-3 col-xs-3 ">
-								<div class="item">
-									<div class="post-thumb">
-										<a title="Điện thoại di động LG G3 16GB D855" href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p28/Dien-thoai-di-dong-LG-G3-16GB-D855.html">
-											<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=28&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/28/01-36-42-12-04-2016-best-smartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=28&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/28/01-38-28-12-04-2016-lgg3androidsmartphone.jpg">
-										</a>
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title">
-												<a href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p28/Dien-thoai-di-dong-LG-G3-16GB-D855.html" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-											</h4>
-											<div class="item-price">
-	                                    				                                    				                                    		<span class="amount-1">7,350,000đ</span>
-													<span class="amount-2">8,500,000đ</span>
-																									    <span class="sale-off">
-												    	-13.5%
-												    </span>
-												    											    	                                    		                                    		</div>
-										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div id="rate-1" style="width:0%" class="rating"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-									</div>
-								</div>
-							</div>
-														<div class="col-lg-3 col-xs-3 ">
-								<div class="item">
-									<div class="post-thumb">
-										<a title="Điện thoại di động LG G3 16GB D855" href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p27/Dien-thoai-di-dong-LG-G3-16GB-D855.html">
-											<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=27&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/27/01-36-42-12-04-2016-best-smartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=27&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/27/01-38-28-12-04-2016-lgg3androidsmartphone.jpg">
-										</a>
-									</div>
-									<div class="item-content">
-										<div class="title-info">
-											<h4 class="post-title">
-												<a href="http://project.vn/BanHang/sieuthigiare.vn/san-pham/p27/Dien-thoai-di-dong-LG-G3-16GB-D855.html" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
-											</h4>
-											<div class="item-price">
-	                                    				                                    				                                    		<span class="amount-1">7,350,000đ</span>
-													<span class="amount-2">8,500,000đ</span>
-																									    <span class="sale-off">
-												    	-13.5%
-												    </span>
-												    											    	                                    		                                    		</div>
-										</div>
-										<div class="item-rating">
-				                            <div class="rating-box">
-				                                <div id="rate-1" style="width:0%" class="rating"></div>
-				                            </div>
-				                        </div>
-				                        <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
-									</div>
-								</div>
-							</div>
-													</div>
-		</div>
+	<div class="title-list-item"><span>Laptop - Máy tính bảng</span></div>
+	<div class="content-list-item w-home">
+		<ul>
+			<?php for($i=0; $i<10; $i++){ ?>
+			<li class="item">
+				<div class="post-thumb">
+					<a title="" href="">
+						<img data-other-src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/31/01-38-28-12-04-2016-lgg3androidsmartphone.jpg" alt="Điện thoại di động LG G3 16GB D855" 
+						src="http://project.vn/BanHang/sieuthigiare.vn/image.php?type_dir=product&amp;id=31&amp;width=300&amp;height=300&amp;image=http://project.vn/BanHang/sieuthigiare.vn/uploads/product/30/01-36-42-12-04-2016-best-smartphone.jpg">
+					</a>
+				</div>
+				<div class="item-content">
+					<div class="title-info">
+						<h4 class="post-title">
+							<a href="" title="Điện thoại di động LG G3 16GB D855">Điện thoại di động LG G3 16GB D855</a>
+						</h4>
+						<div class="item-price">
+	                	<span class="amount-call">Liên hệ: <i class="num-call">0913922986</i></span>
+	                </div>
+					</div>
+					<div class="item-rating">
+	                    <div class="rating-box">
+	                        <div id="rate-1" style="width:0%" class="rating"></div>
+	                    </div>
+	                </div>
+	                <div class="item-banner">Tặng kèm bao da bảo vệ máy</div>
+				</div>
+			</li>
+			<?php } ?>
+		</ul>
 	</div>	
 </div>
 
