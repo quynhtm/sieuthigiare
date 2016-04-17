007429417499184a55e8491b047cc82c7eedc713
trương mạnh quỳnh <quynhtruongmanh@vccorp.vn>
2016-04-17 17:59:13 +0700

    sửa css

--- sites/all/modules/backend/View/tpl/block/admin-content.tpl.php
+++ sites/all/modules/backend/View/tpl/block/admin-content.tpl.php
@@ -15,42 +15,42 @@ global $base_url;
 		<div class="wrapp-content-global">
 			<!--Dòng 1--->
 			<div class="col-lg-2 align_center">
-				<a target="_blank" title="Home Site" href="<?php echo $base_url ?>">
+				<a class="a_control" target="_blank" title="Home Site" href="<?php echo $base_url ?>">
 					<div class="boder_admin padding10">
 						<i class="icon-home icon-4x"></i> <br/>Home Site
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Cấu hình chung" href="<?php echo $base_url ?>/admincp/configinfo">
+				<a class="a_control" title="Cấu hình chung" href="<?php echo $base_url ?>/admincp/configinfo">
 					<div class="boder_admin padding10">
 					<i class="icon-cogs icon-4x"></i> <br/>Cấu hình chung
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý tỉnh thành" href="<?php echo $base_url ?>/admincp/province">
+				<a class="a_control" title="Quản lý tỉnh thành" href="<?php echo $base_url ?>/admincp/province">
 					<div class="boder_admin padding10">
 					<i class="icon-truck icon-4x"></i> <br/>Quản lý tỉnh thành
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Nick hỗ trợ" href="<?php echo $base_url ?>/admincp/supportonline">
+				<a class="a_control" title="Nick hỗ trợ" href="<?php echo $base_url ?>/admincp/supportonline">
 					<div class="boder_admin padding10">
 					<i class="icon-skype icon-4x"></i> <br/>Nick hỗ trợ
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý NCC" href="<?php echo $base_url ?>/admincp/supplier">
+				<a class="a_control" title="Quản lý NCC" href="<?php echo $base_url ?>/admincp/supplier">
 					<div class="boder_admin padding10">
 					<i class="icon-user icon-4x"></i> <br/>Quản lý NCC
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý liên hệ" href="<?php echo $base_url ?>/admincp/contact">
+				<a class="a_control" title="Quản lý liên hệ" href="<?php echo $base_url ?>/admincp/contact">
 					<div class="boder_admin padding10">
 					<i class="icon-envelope icon-4x"></i> <br/>Quản lý liên hệ
 					</div>
@@ -63,28 +63,28 @@ global $base_url;
 			<!--Dòng 2--->
 			<div class="clear paddingTop30"></div>
 			<div class="col-lg-2 align_center">
-				<a title="Danh sách Shop" href="<?php echo $base_url ?>/admincp/usershop">
+				<a class="a_control" title="Danh sách Shop" href="<?php echo $base_url ?>/admincp/usershop">
 					<div class="boder_admin padding10">
-						<i class="icon-group icon-4x"></i> <br/>Danh sách Shop
+						<i class="icon-group icon-4x"></i> <br/>Users Shop
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Danh mục Sản phẩm" href="<?php echo $base_url ?>/admincp/category">
+				<a class="a_control" title="Danh mục Sản phẩm" href="<?php echo $base_url ?>/admincp/category">
 					<div class="boder_admin padding10">
 					<i class="icon-sitemap icon-4x"></i> <br/>Danh mục Sản phẩm
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Banner Quảng cáo" href="<?php echo $base_url ?>/admincp/banner_advanced">
+				<a class="a_control" title="Banner Quảng cáo" href="<?php echo $base_url ?>/admincp/banner_advanced">
 					<div class="boder_admin padding10">
 					<i class="icon-globe icon-4x"></i> <br/>Banner Quảng cáo
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý Tin tức" href="<?php echo $base_url ?>/admincp/news">
+				<a class="a_control" title="Quản lý Tin tức" href="<?php echo $base_url ?>/admincp/news">
 					<div class="boder_admin padding10">
 						<i class="icon-book icon-4x"></i> <br/>Quản lý Tin tức
 					</div>
@@ -95,21 +95,21 @@ global $base_url;
 			<!--Dòng 3--->
 			<div class="clear paddingTop30"></div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý Sản phẩm" href="<?php echo $base_url ?>/admincp/product">
+				<a class="a_control" title="Quản lý Sản phẩm" href="<?php echo $base_url ?>/admincp/product">
 					<div class="boder_admin padding10">
 						<i class="icon-gift icon-4x"></i> <br/>Quản lý Sản phẩm
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Bình luận Sản phẩm" href="<?php echo $base_url ?>/admincp/comments">
+				<a class="a_control" title="Bình luận Sản phẩm" href="<?php echo $base_url ?>/admincp/comments">
 					<div class="boder_admin padding10">
 						<i class="icon-comments icon-4x"></i> <br/>Bình luận Sản phẩm
 					</div>
 				</a>
 			</div>
 			<div class="col-lg-2 align_center">
-				<a title="Quản lý Đơn hàng" href="<?php echo $base_url ?>/admincp/orders">
+				<a class="a_control" title="Quản lý Đơn hàng" href="<?php echo $base_url ?>/admincp/orders">
 					<div class="boder_admin padding10">
 					<i class="icon-shopping-cart icon-4x"></i> <br/>Quản lý Đơn hàng
 					</div>
