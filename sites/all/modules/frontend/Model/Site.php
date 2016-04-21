93ed2b98601beeb7aeb7ef6823b96ed47da5e079
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-21 08:34:37 +0700

    duy

--- sites/all/modules/frontend/Model/Site.php
+++ sites/all/modules/frontend/Model/Site.php
@@ -6,9 +6,59 @@
 * @Version	 : 1.0
 */
 class Site{
-	
-	public static function getListProductContentHome(){
-		$listCat = '';
+	static $table_action_category = TABLE_CATEGORY;
+	static $table_action_product = TABLE_PRODUCT;
+	function makeListCatid($limit=0){
+		global $base_url;
+
+		$arrCatId = array();
+		if($limit > 0){
+			$arrListCat = DB::getItembyCond(self::$table_action_category, 'category_name, category_id', '', 'category_id ASC', 'category_status=1 AND category_content_front=1', $limit);
+			if(!empty($arrListCat)){
+				foreach ($arrListCat as $v){
+				 	$arrListSubCat = DB::getItembyCond(self::$table_action_category, 'category_id', '', 'category_id ASC', 'category_status=1 AND category_parent_id='.$v->category_id, '');
+					if(!empty($arrListSubCat)){
+						foreach($arrListSubCat as $subid){
+							$arrCatId[$v->category_name][] = $subid->category_id;
+						}
+					}
+				}
+			}
+		}else{
+			drupal_goto($base_url);
+		}
+		return $arrCatId;
 	}
 
+
+	public static function getListProductContentHome($limit=0){
+		if($limit > 0){
+			$listCat = Site::makeListCatid($limit);
+			if(!empty($listCat)){
+				foreach($listCat as $k => $v){
+					
+					$sql = db_select(self::$table_action_product, 'i');
+					$sql->addField('i', 'product_name', 'product_name');
+					$sql->addField('i', 'product_price_sell', 'product_price_sell');
+					$sql->addField('i', 'product_price_market', 'product_price_market');
+					$sql->addField('i', 'product_type_price', 'product_type_price');
+					$sql->addField('i', 'product_selloff', 'product_selloff');
+					$sql->addField('i', 'product_image', 'product_image');
+					$sql->addField('i', 'product_image_hover', 'product_image_hover');
+					$sql->condition('i.product_status', STASTUS_SHOW,'=');
+					$sql->condition('i.is_block', PRODUCT_NOT_BLOCK,'=');
+					$sql->condition('i.category_id', $v, 'IN');
+					$sql->orderBy('i.product_id', 'DESC');
+					$sql->range(10);
+					
+					$result[$k] = $sql->execute()->fetchAll();
+				}
+				bug($result);
+				return $result;
+			}
+		}
+		return array();
+	}
 }
+
+Site::getListProductContentHome($limit=20);
