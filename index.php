60fd78422d51801f9a29eea60be7ac18188b03aa
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-22 09:07:00 +0700

    duy

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 try{
 	define('DRUPAL_ROOT', getcwd());
+	
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+
 	require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 	drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 	menu_execute_active_handler();
