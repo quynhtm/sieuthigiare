50038b8a502986f01a3e47341b5e778574db5c7b
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-22 11:36:18 +0700

    duy

--- sites/all/themes/theme_default/page/html.tpl.php
+++ sites/all/themes/theme_default/page/html.tpl.php
@@ -5,7 +5,6 @@
   <?php print t($head); ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body id="<?php print strtolower($body_id); ?>" class="<?php print $classes; ?> body" <?php print $attributes;?>>
   <?php print $page_top; ?>
