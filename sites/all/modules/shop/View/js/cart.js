48aa6b0b69b67ec3630aa4fdbd4af84e6d8fc182
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-26 16:53:26 +0700

    duy

---
+++ sites/all/modules/shop/View/js/cart.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($){
+	SHOP_CART.add();
+});
+SHOP_CART = {
+	add:function(){
+		jQuery('#buttonFormBuySubmit').click(function(){
+			var url = BASEPARAMS.base_url + '/them-vao-gio-hang.html';
+			var pid = jQuery(this).attr('data-pid');
+			var pnum = jQuery('#buy-num').val();
+
+			if(pid>0){
+				jQuery.ajax({
+					type: "POST",
+					url: url,
+					data: "pid="+encodeURI(pid),
+					data: "pid="+encodeURI(pid) + "&pnum="+encodeURI(pnum),
+					success: function(data){
+						return false;
+						if(data=='notAddCart'){
+							jAlert('Không tồn tại sản phẩm!', 'Cảnh báo');
+							return false;
+						}else{
+							jAlert('Đã thêm vào giỏ hàng!', 'Thông báo');
+							window.location.reload();
+						}
+					}
+				});
+			}
+		});
+	},
+
+}
