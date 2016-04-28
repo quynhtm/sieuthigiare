ed19f84ade51d1648cc00862dfec12487ad371f4
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-04-28 14:38:22 +0700

    duy

--- sites/all/modules/frontend/View/tpl/block-header.tpl.php
+++ sites/all/modules/frontend/View/tpl/block-header.tpl.php
@@ -125,9 +125,9 @@
 				</div>
 				<?php } ?>
 			</div>
-			<!-- <div class="desc-price-day">
+			<div class="desc-price-day">
 				<i class="icon-star-empty"></i> <a href="#" title="Giảm giá mỗi ngày">Giảm giá mỗi ngày</a>
-			</div -->
+			</div>
 		</div>
 	</div>
 </div>
